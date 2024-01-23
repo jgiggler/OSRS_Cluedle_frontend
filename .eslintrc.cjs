66dbf2878b7d0061adaae56b3f81b1af427a45f0
Joel Gilger <joelgilger@gmail.com>
2024-01-23 00:41:24 -0800

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,13 +10,11 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', "react"],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    "react/jsx-uses-react": "error",   
-     "react/jsx-uses-vars": "error"
   },
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/OSRS_cluedle_frontend/"
 })
